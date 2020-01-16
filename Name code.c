6f29bf6023c8b0816d4ee1df36e0d2acb0416c61
jackalope67 <50143345+jackalope67@users.noreply.github.com>
2020-01-16 10:29:26 -0600

    Update Name code.c

--- Name code.c
+++ Name code.c
@@ -2,7 +2,7 @@
 int name(void){
 var name = Jack;
 var major = Mechanical Engineering;
-printf("My Name is %d"\n, name);
+printf("My Name is %d", name);
 printf("My Major is %d", major);
        
 return 0;
